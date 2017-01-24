Webstrates/Webstrates

commit f228cb998378f48997368874e1bb8c999cea352a

Fixed issue caused by elements with content property When template support was added (68cdb1cf8d87493d5fc9c394def724322fbd3155), non-template elements with content properties caused issues. Before attempting to access children of a content property, it is now being verified that the content property is in fact a document fragment.

static/lib/jqml.js
@@ -101,7 +101,10 @@
 			// If we just add a documentFragment to an element, the children of documentFragment will
 			// actually be added instead. To prevent this, we add the children to the `content` property
 			// if it exists.
-			(selector.content || selector).appendChild(fragment);
+			if (selector.content && selector.content.nodeType === document.DOCUMENT_FRAGMENT_NODE) {
+				selector = selector.content;
+			}
+			selector.appendChild(fragment);
 			return selector;
 		}
 

static/lib/jsonml-dom.js
@@ -17,10 +17,8 @@ var JsonML = JsonML || {};
 	'use strict';
 
 	var addChildren = function(/*DOM*/ elem, /*function*/ filter, /*JsonML*/ jml) {
-		// elem.content may have childNodes if elem is a template (i.e. elem.content is a document
-		// fragment).
-		if (elem.hasChildNodes() || (elem.content && elem.content.hasChildNodes())) {
-			var childNodes = elem.content ? elem.content.childNodes : elem.childNodes;
+		var childNodes;
+		if (childNodes = webstrates.util.getChildNodes(elem)) {
 			for (var i=0; i<childNodes.length; i++) {
 				var child = childNodes[i];
 				child = fromHTML(child, filter);
@@ -69,7 +67,7 @@ var JsonML = JsonML || {};
 					jml.push(props); //Webstrates always assumes that an element has attributes.
                 //}
 
-				var child;
+				var child, childNodes;
 				switch (jml[0].toLowerCase()) {
 					case 'frame':
 					case 'iframe':
@@ -98,8 +96,9 @@ var JsonML = JsonML || {};
 							// unwrap comment blocks
 							child = child.replace('<!--', '').replace('-->', '');
 							jml.push(child);
-						} else if (elem.hasChildNodes() || (elem.content && elem.content.hasChildNodes())) {
-							var childNodes = elem.content ? elem.content.childNodes : elem.childNodes;
+						// elem.content may have childNodes if elem is a template (i.e. elem.content is a
+						// document fragment).
+						} else if (childNodes = webstrates.util.getChildNodes(elem)) {
 							for (i=0; i<childNodes.length; i++) {
 								child = childNodes[i];
 								child = fromHTML(child, filter);

static/webstrates.applyop.js
@@ -90,14 +90,9 @@ root.webstrates = (function(webstrates) {
 		var newElement = typeof value === 'string' ?
 			document.createTextNode(value) : jqml(value, namespace);
 
-		// If parentElement.content exists, parentElement contains a documentFragment, and we should be
-		// adding the content to this documentFragment instead. This happens when parentElement is a
-		// <template>.
-		if (childElement) {
-			(parentElement.content || parentElement).insertBefore(newElement, childElement);
-		} else {
-			webstrates.util.appendChildWithoutScriptExecution((parentElement.content || parentElement), newElement);
-		}
+		// childElement may be undefined, and if so we insert newElement at the end of the list. If
+		// chidElement is defined, however, we insert the element before childElement.
+		webstrates.util.appendChildWithoutScriptExecution(parentElement, newElement, childElement);
 
 		var parentPathNode = webstrates.PathTree.getPathNode(parentElement);
 		var childPathNode = new webstrates.PathTree(newElement, parentPathNode);
@@ -114,7 +109,7 @@ root.webstrates = (function(webstrates) {
 	var deleteNode = function(parentElement, path) {
 		var [head, ...tail] = path;
 		var key = head - jsonml.ELEMENT_LIST_OFFSET;
-		var childElement = (parentElement.content || parentElement).childNodes[key];
+		var childElement = webstrates.util.getChildNodes(parentElement)[key];
 
 		if (tail.length > 0) {
 			return deleteNode(childElement, tail);
@@ -172,7 +167,7 @@ root.webstrates = (function(webstrates) {
 
 			// Overwrite old node with new node.
 			parentPathNode.children.splice(key, 1, newElementPathNode);
-			parentElement.insertBefore(newElement, oldElement);
+			appendChildWithoutScriptExecution(parentElement, newElement, oldElement);
 			oldElement.remove();
 			return;
 		}

static/webstrates.util.js
@@ -33,8 +33,9 @@ root.webstrates = (function(webstrates) {
 
 	/**
 	 * Append a DOM element childElement to another DOM element parentElement. If the DOM element to
-	 * be appended is a script, prevent the execution of the script. If a referenceNode is specified,
-	 * the element is inserted before the referenceNode.
+	 * be appended is a script, prevent the execution of the script. If the parentElement is a
+	 * <template>, add the child to the parentElement's documentFragment instead. If a referenceNode
+	 * is specified, the element is inserted before the referenceNode.
 	 * @param {DOMNode} parentElement Parent element.
 	 * @param {DOMNode} childElement  Child element.
 	 */
@@ -45,9 +46,22 @@ root.webstrates = (function(webstrates) {
 			parentElement.insertBefore(childElement, referenceNode || null);
 			childElement.innerHTML = script;
 		} else {
+			// If parentElement.content exists, parentElement contains a documentFragment, and we should
+			// be adding the content to this documentFragment instead. This happens when parentElement is
+			// a <template>.
+			if (parentElement.content && parentElement.content === document.DOCUMENT_FRAGMENT_NODE) {
+				parentElement = parentElement.content;
+			}
 			parentElement.insertBefore(childElement, referenceNode || null);
 		}
 	};
+
+	util.getChildNodes = function(parentElement) {
+		if (parentElement.content && parentElement.content === document.DOCUMENT_FRAGMENT_NODE) {
+			parentElement = parentElement.content;
+		}
+		return parentElement.childNodes;
+	}
 
 	/**
 	 * Traverses an element tree and applies a callback to each element.
@@ -58,8 +72,7 @@ root.webstrates = (function(webstrates) {
 	util.recursiveForEach = function(element, callback) {
 		callback(element);
 
-		var childNodes = element.content ? element.content.childNodes : element.childNodes;
-		Array.from(childNodes).forEach(function(childNode) {
+		Array.from(util.getChildNodes(element)).forEach(function(childNode) {
 			util.recursiveForEach(childNode, callback);
 		});
 	};

commit 1d23784f6844a5983faf934bf0b0385f06a02067

Fixed template bug regression from f228cb998378f48997368874e1bb8c999cea352a Bug causing a <template>'s content (rather than the template itself) to be inserted into the DOM by jqml.js. This happened, because we previously returned selector.content (i.e. the template's document fragment) rather than the template itself (selector) from createObj(), and thus only the document fragment was inserted.

static/lib/jqml.js
@@ -132,9 +132,10 @@
 			// actually be added instead. To prevent this, we add the children to the `content` property
 			// if it exists.
 			if (selector.content && selector.content.nodeType === document.DOCUMENT_FRAGMENT_NODE) {
-				selector = selector.content;
+				selector.content.appendChild(fragment);
+			} else {
+				selector.appendChild(fragment);
 			}
-			selector.appendChild(fragment);
 			return selector;
 		}
 