Webstrates/Webstrates

commit 60e52001066fff406d22f1a1512e8c6fd51ebb45

Now persists transient elements defined in templates. Adding a `<transient>` tag to a `<template>` will now persist the `<transient>` tag. Once the template is instantiated/added into the document, the transient element will behave as per usual and thus only exist in the client that instantiated it.

static/webstrates.createops.js
@@ -168,19 +168,18 @@ root.webstrates = (function(webstrates) {
 			}
 
 			var newPathNode = webstrates.PathTree.create(addedNode, targetPathNode);
-
 			if (!newPathNode) {
 				return;
 			}
 
 			// We use the previous sibling to insert the new element in the correct position in the path
-			// tree. However, if the previous sibling is a transient element, it won't be in the path
-			// tree, so it will appear that the element has no previous element. Therefore, we traverse
-			// the list of previous siblings until we find one that's not a transient element (if such
-			// exists).
+			// tree. However, if the previous sibling doesn't have a webstrate object, it won't be in the
+			// path tree, so it will appear that the element has no previous element. Therefore, we
+			// traverse the list of previous siblings until we find one that does have a webstrate object.
+			// Transient elements (outside of template tags) will righfully be absent from the pathtree,
+			// and thus not have webstrate objects.
 			var previousSibling = mutation.previousSibling;
-			while (previousSibling && previousSibling.tagName
-				&& previousSibling.tagName.toLowerCase() === "transient") {
+			while (previousSibling && previousSibling.webstrate) {
 				previousSibling = previousSibling.previousSibling;
 			}
 
@@ -195,7 +194,11 @@ root.webstrates = (function(webstrates) {
 				targetPathNode.children.push(newPathNode);
 			}
 
-			mutation.target.webstrate.fireEvent("nodeAdded", addedNode, true);
+			// If no webstrate object exists, we fire the event on the parent instead. This will be the
+			// case when changes are applied to a <template>'s documentFragment (in which case we fire the
+			// event on the containing <template>).
+			(mutation.target.webstrate || newPathNode.parent.DOMNode.webstrate)
+				.fireEvent("nodeAdded", addedNode, true);
 
 			var path = webstrates.PathTree.getPathNode(addedNode, target).toPath();
 			var op = { li: JsonML.fromHTML(addedNode), p: path };
@@ -210,7 +213,11 @@ root.webstrates = (function(webstrates) {
 				return;
 			}
 
-			mutation.target.webstrate.fireEvent("nodeRemoved", removedNode, true);
+			// If no webstrate object exists, we fire the event on the parent instead. This will be the
+			// case when changes are applied to a <template>'s documentFragment (in which case we fire the
+			// event on the containing <template>).
+			(mutation.target.webstrate || removedPathNode.parent.DOMNode.webstrate)
+				.fireEvent("nodeRemoved", removedNode, true);
 
 			var path = removedPathNode.toPath();
 			removedPathNode.remove();

static/webstrates.js
@@ -708,6 +708,7 @@ root.webstrates = (function(webstrates) {
 			 * @private
 			 */
 			var mutationToOps = function(mutations) {
+
 				mutations.forEach(function forEachMutation(mutation) {
 					var ops = webstrates.createOps(mutation, doc, fragmentParentMap);
 					// In rare cases, what happens doesn't amount to an operation, so we ignore it. See the

static/webstrates.pathtree.js
@@ -64,6 +64,19 @@ root.webstrates = (function(webstrates) {
 	};
 
 	/**
+	 * Check whether an element is a descendant of a template tag (or actually a documentFragment).
+	 * One might assume this could be done with `element.closest("template")`, but that won't be the
+	 * case, because a documentFragment technically isn't a parent (and also doesn't have any parent),
+	 * so there will be no tree to search upwards through after we reach the documentFragment.
+	 * @param  {DOMNode} element Element to check.
+	 * @return {boolean}         True if the element is a descendant of a template.
+	 * @private
+	 */
+	var elementIsTemplateDescendant = function(element) {
+		return document.body.ownerDocument !== element.ownerDocument;
+	};
+
+	/**
 	 * Add PathNode to node if the node isn't a <transient> element.
 	 * @param  {[type]} DOMNode        [description]
 	 * @param  {[type]} parentPathTree [description]
@@ -72,8 +85,9 @@ root.webstrates = (function(webstrates) {
 	 */
 	PathTree.create = function(DOMNode, parentPathTree, overwrite) {
 		// Transient elements are not supposed to be persisted, and should thus not be part of the
-		// PathTree.
-		if (DOMNode.tagName && DOMNode.tagName.toLowerCase() === "transient") {
+		// PathTree. Unless the transient element is in a <template>.
+		if (DOMNode.tagName && DOMNode.tagName.toLowerCase() === "transient" &&
+			!elementIsTemplateDescendant(DOMNode)) {
 			return;
 		}
 
@@ -168,7 +182,8 @@ root.webstrates = (function(webstrates) {
 		var childNodes = this.DOMNode.hasChildNodes() ? this.DOMNode.childNodes
 				: (this.DOMNode.content && this.DOMNode.content.childNodes) || [];
 		var childNodes = Array.from(childNodes).filter(function(childNode) {
-			return !childNode.tagName || childNode.tagName.toLowerCase() !== "transient";
+			return !childNode.tagName || childNode.tagName.toLowerCase() !== "transient"
+				|| elementIsTemplateDescendant(childNode);
 		});
 		if (definedChildNodesInDom.length !== childNodes.length) {
 			console.log(definedChildNodesInDom, childNodes);

static/webstrates.util.js
@@ -170,7 +170,8 @@ root.webstrates = (function(webstrates) {
 			// If parentElement.content exists, parentElement contains a documentFragment, and we should
 			// be adding the content to this documentFragment instead. This happens when parentElement is
 			// a <template>.
-			if (parentElement.content && parentElement.content === document.DOCUMENT_FRAGMENT_NODE) {
+			if (parentElement.content &&
+				parentElement.content.nodeType === document.DOCUMENT_FRAGMENT_NODE) {
 				parentElement = parentElement.content;
 			}
 			parentElement.insertBefore(childElement, referenceNode || null);

commit 0788bc757ec943666e17290c6e06b1c00cac1386

Fixed shuffle bug regression reintroduced in 60e52001066fff406d22f1a1512e8c6fd51ebb45

static/webstrates.createops.js
@@ -179,7 +179,7 @@ root.webstrates = (function(webstrates) {
 			// Transient elements (outside of template tags) will righfully be absent from the pathtree,
 			// and thus not have webstrate objects.
 			var previousSibling = mutation.previousSibling;
-			while (previousSibling && previousSibling.webstrate) {
+			while (previousSibling && !previousSibling.webstrate) {
 				previousSibling = previousSibling.previousSibling;
 			}
 