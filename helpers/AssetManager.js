Webstrates/Webstrates

commit b4c3ea148984aa4450d22319c1ac00223fe8e585

Announce new searchable assets correctly

helpers/AssetManager.js
@@ -350,11 +350,6 @@ module.exports.addAsset = async function(webstrateId, asset, searchable, source)
 		fileHash: asset.fileHash
 	});
 
-	if (searchable && asset.mimetype === 'text/csv') {
-		const assetId = result.insertedId;
-		await searchableAssets.makeSearchable(assetId, module.exports.UPLOAD_DEST + asset.filename);
-	}
-
 	asset = {
 		v: version,
 		fileName: asset.originalname,
@@ -363,6 +358,12 @@ module.exports.addAsset = async function(webstrateId, asset, searchable, source)
 		identifier: asset.filename,
 		fileHash: asset.fileHash
 	};
+
+	if (searchable && asset.mimetype === 'text/csv') {
+		const assetId = result.insertedId;
+		await searchableAssets.makeSearchable(assetId, module.exports.UPLOAD_DEST + asset.filename);
+		asset.searchable = true;
+	}
 
 	// Inform all clients of the newly added asset.
 	clientManager.announceNewAsset(webstrateId, asset, true);

commit f0c413331a1ee5ee522f5aba6e69019548ddd189

Fix filename bug introduced in b4c3ea1

helpers/AssetManager.js
@@ -350,7 +350,7 @@ module.exports.addAsset = async function(webstrateId, asset, searchable, source)
 		fileHash: asset.fileHash
 	});
 
-	asset = {
+	const assetToBeAnnounced = {
 		v: version,
 		fileName: asset.originalname,
 		fileSize: asset.size,
@@ -362,13 +362,13 @@ module.exports.addAsset = async function(webstrateId, asset, searchable, source)
 	if (searchable && asset.mimetype === 'text/csv') {
 		const assetId = result.insertedId;
 		await searchableAssets.makeSearchable(assetId, module.exports.UPLOAD_DEST + asset.filename);
-		asset.searchable = true;
+		assetToBeAnnounced.searchable = true;
 	}
-
+	
 	// Inform all clients of the newly added asset.
-	clientManager.announceNewAsset(webstrateId, asset, true);
+	clientManager.announceNewAsset(webstrateId, assetToBeAnnounced, true);
 
-	return asset;
+	return assetToBeAnnounced;
 };
 
 /**